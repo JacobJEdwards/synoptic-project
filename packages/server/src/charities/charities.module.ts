4af4378fc8ee49c872c172f2f277462ab82032ff
JacobJEdwards <jacob.john1@talktalk.net>
2023-05-31 16:03:26 +0100

    fixed bug

--- packages/server/src/charities/charities.module.ts
+++ packages/server/src/charities/charities.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CharitiesService } from './charities.service';
 import { CharitiesController } from './charities.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [CharitiesController],
   providers: [CharitiesService],
-  imports: [PrismaService]
+  imports: [PrismaModule]
 })
 export class CharitiesModule {}
