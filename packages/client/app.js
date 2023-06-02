e8b413f1f33116cedc0fa9c93d5a0eab1b1ade6d
JacobJEdwards <jacob.john1@talktalk.net>
2023-06-02 01:32:07 +0100

    updated express server

--- packages/client/app.js
+++ packages/client/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const { createServer } = require("http");
 
 // import middleware
 const morgan = require("morgan");
@@ -8,29 +9,42 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 // setup express app
-const app = express();
 const port = process.env.PORT || 3001;
 
+const app = express();
+app.set("port", port);
+
 // add middleware
-app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
+app.use(compression());
+app.use(morgan("dev"));
 
 app.use(express.static("src"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(compression());
+// error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Internal Server Error");
+})
 
 app.get("*", (req, res) => {
   res.send("Working");
 });
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err);
-  }
+const server = createServer(app);
+server.listen(port);
 
-  console.log(`Listening on port ${port}`);
+server.on("error", (err) => {
+  console.log(err);
+});
+
+server.on("listening", () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`Server is listening at http://${host}:${port}`);
 });
