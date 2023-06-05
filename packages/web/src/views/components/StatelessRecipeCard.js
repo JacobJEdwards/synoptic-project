121bf0f7b7e5a1fe7ba58f96c36c8092f3f69070
JacobJEdwards <jacob.john1@talktalk.net>
2023-06-05 13:55:06 +0100

    updates

--- packages/web/src/views/components/StatelessRecipeCard.js
+++ packages/web/src/views/components/StatelessRecipeCard.js
@@ -2,29 +2,32 @@ import Link from "./Link.js";
 import Component from "./StatelessComponent.js";
 
 export default class StatelessCharityCard extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        const recipe = this.props;
-        if (!recipe) return "";
+  render() {
+    const recipe = this.props;
+    if (!recipe) return "";
 
-        const link = new Link({
-            href: `/recipes/${recipe.id}`,
-            text: "View Recipe",
-        });
+    const link = new Link({
+      href: `/recipes/${recipe.id}`,
+      text: "View Recipe",
+    });
 
-        return `
+    return `
+            <article class="services">
                 <figure>
-                    <img src="/views/images/placeholder.jpg" alt="${recipe.name
-            }" />
+                    <img src="/views/images/placeholder.jpg" alt="${
+                      recipe.name
+                    }" />
                     <figcaption>
                         <h2>${recipe.title}</h2>
                         <p>${recipe.description}</p>
                         ${link.render()}
                     </figcaption>
                 </figure>
+                </article>
             `;
-    }
+  }
 }
