ed47b086ba928426b53720ba8c8eb8f042a07e5b
JacobJEdwards <jacob.john1@talktalk.net>
2023-06-11 23:25:37 +0100

    moved charty html

--- packages/web/src/views/pages/Charities.ts
+++ packages/web/src/views/pages/Charities.ts
@@ -1,110 +1,104 @@
-import {AbstractPage as Page} from "@lib/components"
-import type {Charity, LoaderFunction, Params} from "@lib/types"
+import { AbstractPage as Page } from "@lib/components";
+import type { Charity, LoaderFunction, Params } from "@lib/types";
 
-import {getCharities} from "@services/charity.service";
+import { getCharities } from "@services/charity.service";
 import CharityCard from "@components/StatelessCharityCard";
 
 export const loader: LoaderFunction<Charity[] | null> = async () => {
-    const charities = await getCharities();
+  const charities = await getCharities();
 
-    if (!charities) return {
-        success: false,
-        data: null,
-        error: "Failed to load charities",
-    }
-
+  if (!charities)
     return {
-        success: true,
-        data: charities,
-    }
+      success: false,
+      data: null,
+      error: "Failed to load charities",
+    };
+
+  return {
+    success: true,
+    data: charities,
+  };
 };
 
 export default class Charities extends Page {
-    constructor(params: Params, title = "Charities") {
-        super(params, title);
+  constructor(params: Params, title = "Charities") {
+    super(params, title);
+  }
+
+  async getHtml() {
+    let charities = this.loaderData?.data as Charity[];
+
+    const additonalCharities = [
+      {
+        id: 100,
+        name: "Get Into Govan",
+        description:
+          "This group is made up of many organisations listed below, along with local community members. They are working together as a group to achieve better food outcomes in Govan. Aberlour,  Elderpark Housing Association, Gilded Lily, Glasgow City Council Neighbourhoods & Sustainability (Growing Spaces), Glasgow City HSCP (Health Improvement),  Glasgow Community Food Network, Glasgow Housing Association, Glasgow Life, Go Vegan Govan, Govan Boxing Club, Govan Community Project, Govan HELP, Govan Housing Association, Govan Youth Information Point,  Moogety Foods, Preshal Trust, Propagate, The Riverside Hall, and Urban Roots.",
+        website:
+          "https://getintogovan.com/thriving-places-food-for-good-group/",
+      },
+      {
+        id: 101,
+        name: "Moogety Foods",
+        description:
+          "Moogety Foods is a non-profit social enterprise based in Govan, Glasgow, promoting healthy eating, cooking and participation within the community.",
+        website:
+          "https://elderpark.org/your-community/projects/moogety-foods-moogety-food-hub-moogety-garden/",
+      },
+      {
+        id: 102,
+        name: "Govan Pantry",
+        description:
+          "The Govan Pantry is a subsidised community shop that helps families to shop smarter and budget better, providing access to food and other essential items at reduced cost.",
+        website: "https://www.govanhelp.org/services/the-govan-pantry",
+      },
+    ] as Charity[];
+
+    if (charities) {
+      charities.unshift(...additonalCharities);
+    } else {
+      charities = additonalCharities;
     }
 
-    async getHtml() {
-        const charities = this.loaderData?.data;
-        let charitiesHtml = "";
+    let charitiesHtml = "";
 
-        if (charities) {
-            charitiesHtml = charities
-                .map((charity: Charity) => {
-                    return new CharityCard(charity).render();
-                })
-                .join("");
-        }
+    if (charities) {
+      charitiesHtml = charities
+        .map((charity: Charity) => {
+          return new CharityCard(charity).render();
+        })
+        .join("");
+    }
 
-        const view = `
+    const view = `
         <section class="prose">
         <h1>Charities Page</h1>
-        <section class='box'>
-          <article class='float-left'>
-              <p>
-                <span class="charity-info">Get Into Govan:</span><br><br> This group is made up of many organisations listed below, along with local community members. They are working together as a group to achieve better food outcomes in Govan. 
-                Aberlour,  Elderpark Housing Association, Gilded Lily, Glasgow City Council Neighbourhoods & Sustainability (Growing Spaces), Glasgow City HSCP (Health Improvement),  Glasgow Community Food Network, Glasgow Housing Association, Glasgow Life, Go Vegan Govan, Govan Boxing Club, Govan Community Project, Govan HELP, Govan Housing Association, Govan Youth Information Point,  Moogety Foods, Preshal Trust, Propagate, The Riverside Hall, and Urban Roots."
-              </p>
-              <a href="https://getintogovan.com/thriving-places-food-for-good-group/">
-              <button class="charity-button">Get Into Govan</button></a>
-          </article>
-          <div class='float-right'>
-            <img src='views/images/getintogovan.jpeg'>
-          </div>
-        </section>
-  
-        <section class='box'>
-          <br>
-          <article class='float-left'>
-            <p>
-              <span class="charity-info">Moogety Foods:</span><br><br> Moogety Foods is a non-profit social enterprise based in Govan, Glasgow, promoting healthy eating, cooking and participation within the community."
-            </p>
-            <a href="https://elderpark.org/your-community/projects/moogety-foods-moogety-food-hub-moogety-garden/">
-            <button class="charity-button">Moogety Foods</button></a>
-          </article>
-          <div class='float-right'>
-            <img src='views/images/moogetyfoods.jpeg'>
-          </div>
-        </section>
-  
-        <section class='box'>
-          <article class='float-left'>
-            <p>
-              <span class="charity-info">Govan Pantry:</span><br><br> The Govan Pantry is a subsidised community shop that helps families to shop smarter and budget better, providing access to food and other essential items at reduced cost."
-            </p>
-            <a href="https://www.govanhelp.org/services/the-govan-pantry">
-            <button class="charity-button">Govan Pantry</button></a>
-          </article>
-          <div class='float-right'>
-            <img src='views/images/govanpantry.png'>
-          </div>
-        </section>
-        ${charitiesHtml}
+            ${charitiesHtml}
       </section>
         `;
 
-        return view;
-    }
+    return view;
+  }
 
-    // probably could move to get html
-    // ssr
-    // async clientScript() {
-    //     const charities = this.loaderData;
-    //     if (!charities) return;
-    //
-    //     const charityContainer = document.querySelector(".charity-container");
-    //
-    //     charities.forEach((charity) => {
-    //         const charityElement = document.createElement("article");
-    //         charityElement.classList.add("service");
-    //
-    //         const charityComponent = new CharityCard(charityElement, charity);
-    //         charityComponent.init();
-    //
-    //         charityContainer.appendChild(charityElement);
-    //     });
+  // probably could move to get html
+  // ssr
+  // async clientScript() {
+  //     const charities = this.loaderData;
+  //     if (!charities) return;
+  //
+  //     const charityContainer = document.querySelector(".charity-container");
+  //
+  //     charities.forEach((charity) => {
+  //         const charityElement = document.createElement("article");
+  //         charityElement.classList.add("service");
+  //
+  //         const charityComponent = new CharityCard(charityElement, charity);
+  //         charityComponent.init();
+  //
+  //         charityContainer.appendChild(charityElement);
+  //     });
 
-    async clientScript() {
-        return;
-    }
+  async clientScript() {
+    return;
+  }
 }
