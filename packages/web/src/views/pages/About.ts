9ba0360709fd7211c0ac51c5aafa5cbf4d70f28c
Neiljohn Deligero <79201301+neiljanjan@users.noreply.github.com>
2023-06-12 14:54:42 +0100

    added styling to about us page

--- packages/web/src/views/pages/About.ts
+++ packages/web/src/views/pages/About.ts
@@ -9,8 +9,46 @@ export default class About extends Page {
     override async getHtml() {
         const view = `
         <section class="prose">
-      <h1 class="text-2xl">About Us Page</h1>
-      </section>
+        <h1 class="text-2xl">About Us Page</h1>
+        <section class='about_box'>
+            <h1>
+                Our team
+            </h1>
+            <br>
+            <div class='about_content'>
+                <ul>
+                    <li>Jacob</li>
+                    <li>Josh</li>
+                    <li>Neil</li>
+                    <li>Brandon</li>
+                    <li>Henry</li>
+                    <li>Alfie</li>
+                </ul>
+            <div>
+        </section>
+        <section class='about_box'>
+        <h1>
+            What the website is about
+        </h1>
+        <br>
+        <div class='about_content'>
+            <p>
+                This website is designed to showcase recepies from around the world. This is where people can upload and exchange their own recepies, while also learning new recepies from different cultures.
+            </p>
+        </div>
+        </section>
+        <section class='about_box'>
+        <h1>
+            How our website helps the community
+        </h1>
+        <br>
+        <div class='about_content'>
+            <p>
+                This website will help the community to learn about the different cultures and dishes from around the world. This will also help to give budget friendly ideas for their meals.
+            </p>
+        </div>
+    </section>
+        </section>
         `;
 
         return view;
